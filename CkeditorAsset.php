e866e15e896c5518b3d80a21e2b61525627fac20
cuileon <cuiliang@live.cn>
2017-07-30 14:55:09 +0800

    add assets

---
+++ CkeditorAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiichina\ckeditor;
+
+use yii\web\AssetBundle;
+
+/**
+ * This asset bundle provides the javascript files for client validation.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class CkeditorAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/ckeditor';
+
+    public $js = [
+        'ckeditor.js',
+    ];
+
+    public $depends = array(
+        'yii\bootstrap\BootstrapAsset'
+    );
+}
